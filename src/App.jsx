91958c96c6cbe542daa73983f55793285873c706
jhon.ricardo.rios.cuervo <jhonri.0614@gmail.com>
2025-10-23 19:55:08 -0500

    Creando la tarjeta del aprendiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,25 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React primer proyecto adso React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 10)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
-//mofidicado
+//primer codigo
+import './App.css'
+import TarjetaAprendiz from "./components/TarjetaAprendiz";
 export default function App() {
-  const fecha = new Date().toLocaleDateString();
-  return ( <main> <h1>Hola SENA</h1></main>)};
+return (
+<main style={{
+maxWidth: 520,
+margin: "30px auto",
+fontFamily: "sans-serif"
+}}>
+<h2>Aprendices ADSO</h2>
+<TarjetaAprendiz
+nombre="Jhon"
+ficha={3223876}
+programa="ADSO"
+/>
+
+<TarjetaAprendiz
+nombre="Juan"
+ficha={3144585}
+programa="ADSO"
+/>
+</main>
+);
+}
