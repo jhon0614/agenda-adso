549ca11df6e5473775f0f299899076db35425b15
jhon.ricardo.rios.cuervo <jhonri.0614@gmail.com>
2025-11-06 19:12:23 -0500

    Agregando campo de empresa (Clase_5_Agenda_ADSO_v3_UI)

--- src/components/FormularioContacto.jsx
+++ src/components/FormularioContacto.jsx
@@ -3,53 +3,59 @@ import { useState } from "react";
 export default function FormularioContacto({ onAgregar }) {
   const [form, setForm] = useState({
     nombre: "",
-    correo: "",
     telefono: "",
+    correo: "",
     etiqueta: "",
+    empresa: ""
   });
 
+  // Handler genérico para inputs
   const onChange = (e) => {
     const { name, value } = e.target;
     setForm((f) => ({ ...f, [name]: value }));
   };
 
+  // Enviar datos al padre
   const onSubmit = (e) => {
-    e.preventDefault(); // evita recargar la página
-    if (!form.nombre.trim() || !form.telefono.trim()) {
-      alert("Completa al menos Nombre y Teléfono");
-      return;
-    }
-    onAgregar(form); // App agrega id y actualiza la lista
-    setForm({ nombre: "", correo: "", telefono: "", etiqueta: "" }); // limpiar
+    e.preventDefault();
+    if (!form.nombre || !form.telefono || !form.correo) return; // Validación mínima
+    onAgregar(form); // Entregamos al padre
+    setForm({ nombre: "", telefono: "", correo: "", etiqueta: "", empresa: "" }); // Reset
   };
 
   return (
-    <form onSubmit={onSubmit} className="form-contacto">
+    <form className="form-contacto" onSubmit={onSubmit}>
       <input
         name="nombre"
-        placeholder="Nombre"
         value={form.nombre}
         onChange={onChange}
+        placeholder="Nombre"
       />
       <input
         name="telefono"
-        placeholder="Teléfono"
         value={form.telefono}
         onChange={onChange}
+        placeholder="Teléfono"
       />
       <input
         name="correo"
-        placeholder="Correo"
         value={form.correo}
         onChange={onChange}
+        placeholder="Correo"
       />
       <input
         name="etiqueta"
-        placeholder="Etiqueta (opcional)"
         value={form.etiqueta}
         onChange={onChange}
+        placeholder="Etiqueta opcional"
       />
-      <button type="submit">Agregar contacto</button>
+      <input
+        name="empresa"
+        value={form.empresa}
+        onChange={onChange}
+        placeholder="Empresa opcional"
+      />
+      <button className="btn-agregar">Agregar contacto</button>
     </form>
   );
 }
