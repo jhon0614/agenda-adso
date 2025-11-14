861d1b2048f7f3b3f7eac39591c078d802f5ba56
jhon.ricardo.rios.cuervo <jhonri.0614@gmail.com>
2025-11-13 19:05:35 -0500

    Clase_7_Agenda_ADSO_v5_API

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <--- IMPORTANTE: estilos con Tailwind
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Importamos el componente raíz
+import App from "./App.jsx";
+// Importante: trae Tailwind a la app
+import "./index.css";
+// Punto de entrada de la app: renderiza <App /> dentro de #root
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
