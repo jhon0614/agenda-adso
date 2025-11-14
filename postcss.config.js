3d1edabb2c5fd744eeae5dc17cdee03532557b2c
jhon.ricardo.rios.cuervo <jhonri.0614@gmail.com>
2025-11-13 19:06:26 -0500

    Implementación de tailwind e instalación de json-server

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+// Configuración de PostCSS para Tailwind v3.4 (estable)
+
+// Instructor: Gustavo Bolaños – Curso: Desarrollo Web – ReactJS (Agenda ADSO)
 export default {
   plugins: {
+    // Registra Tailwind como plugin de PostCSS
     tailwindcss: {},
+    // Autoprefixer agrega compatibilidad de prefijos CSS
     autoprefixer: {},
   },
 };
